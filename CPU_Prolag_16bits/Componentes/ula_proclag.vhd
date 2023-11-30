10af3ff52e84bd21dbe081242eb52df96eea8b04
Guilherme Almeida <107274359+GuilhermeAlmeidadaLuz@users.noreply.github.com>
2023-11-30 00:26:44 -0400

    Update ula_proclag.vhd

--- CPU_Prolag_16bits/Componentes/ula_proclag.vhd
+++ CPU_Prolag_16bits/Componentes/ula_proclag.vhd
@@ -1,8 +1,4 @@
--- Referência:
---
-
-
---
+-- 
 -- ULA (UAL) do processador
 --
 LIBRARY IEEE;
@@ -29,24 +25,24 @@ BEGIN
 	CASE ula_controle IS -- define a operação da ULA (funct)
 	-- funct
 	WHEN "000" =>
-	resultado <= a + b; -- operação de adição (add)
+		resultado <= a + b; -- operação de adição (add)
 	WHEN "001" =>
-	resultado <= a - b; -- operação de subtração (sub)
+		resultado <= a - b; -- operação de subtração (sub)
 	--
 	WHEN "010" => 
-	resultado <= a and b; -- and
+		resultado <= a and b; -- and
 	WHEN "011" =>
-   resultado <= a or b; -- or
+   		resultado <= a or b; -- or
    --
 	WHEN "100" => -- verificação de beq (branch equal)
-   IF (a<b xor a>b) THEN 
-   resultado <= x"0001"; -- flag para a < b ou a > b
-   ELSE
-   resultado <= x"0000"; -- flag para a = b
-   END IF;
+   		IF (a<b xor a>b) THEN 
+   			resultado <= x"0001"; -- flag para a < b ou a > b
+   		ELSE
+   			resultado <= x"0000"; -- flag para a = b
+   		END IF;
 	--
 	WHEN OTHERS =>
-	resultado <= a + b; -- add
+		resultado <= a + b; -- add
 	--
 	END CASE;
 END PROCESS;
