fc13ed77133315dc1317933f60014d356b248515
Guilherme Almeida <107274359+GuilhermeAlmeidadaLuz@users.noreply.github.com>
2023-11-30 00:18:51 -0400

    Add files via upload
    Folder for CPU_prolag components

---
+++ CPU_Prolag_16bits/Componentes/ram_proclag.vhd
@@ -0,0 +1,37 @@
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.all;
+USE IEEE.numeric_std.all;
+
+ENTITY ram_proclag IS
+PORT(
+		Clock, ram_LerMem, ram_EscMem: in std_logic; -- clock e flags da UC
+		ram_end_acessado: in std_logic_vector(16-1 downto 0);
+		ram_dado_escrever: in std_logic_vector(16-1 downto 0);
+		ram_dado_lido: out std_logic_vector(16-1 downto 0)
+		);
+END ram_proclag;
+
+ARCHITECTURE procedimento OF ram_proclag IS
+
+SIGNAL int: integer;
+SIGNAL ram_endereco: std_logic_vector(8-1 downto 0);
+
+TYPE mem_de_dados is array (0 to 255) OF std_logic_vector(16-1 downto 0); -- memoria de dados é array 2^8 e em cada posição/célula recebe 16 bits que tem 65535 combinações
+
+SIGNAL RAM: mem_de_dados := ( (others => (others => '0') ) ); -- mesmo que mem_de_dados := others => "0000000000000000"
+
+BEGIN
+
+	ram_endereco <= ram_end_acessado (8 downto 1);
+	
+	PROCESS(clock)
+	
+		BEGIN
+			IF(rising_edge(clock)) THEN -- crista da onda
+				IF(ram_EscMem = '1') THEN
+				RAM(to_integer ( unsigned (ram_endereco) ) ) <= ram_dado_escrever;
+				END IF;
+			END IF;
+	END PROCESS;
+		ram_dado_lido <= RAM( to_integer (unsigned (ram_endereco) ) ) WHEN (ram_LerMem = '1') ELSE x"0000";
+END procedimento;
---
+++ CPU_Prolag_16bits/Componentes/ula_proclag.vhd
@@ -0,0 +1,55 @@
+-- Referência:
+--
+
+
+--
+-- ULA (UAL) do processador
+--
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.all;
+USE IEEE.std_logic_signed.all;
+USE ieee.std_logic_arith.all;
+
+ENTITY ula_proclag IS
+PORT(
+	a, b : in std_logic_vector(16-1 downto 0); -- entrada para registradores
+	ula_controle : in std_logic_vector(3-1 downto 0); -- seletor de função (funct)
+	ula_resultado : out std_logic_vector(16-1 downto 0); -- resultado de saída da ula
+	zero : out std_logic -- bandeira/sinalizador zero
+	);
+END ula_proclag;
+
+ARCHITECTURE Comportamento OF ula_proclag IS -- código
+SIGNAL resultado : std_logic_vector(16-1 downto 0);
+
+
+BEGIN
+PROCESS(ula_controle, a, b)
+BEGIN
+	CASE ula_controle IS -- define a operação da ULA (funct)
+	-- funct
+	WHEN "000" =>
+	resultado <= a + b; -- operação de adição (add)
+	WHEN "001" =>
+	resultado <= a - b; -- operação de subtração (sub)
+	--
+	WHEN "010" => 
+	resultado <= a and b; -- and
+	WHEN "011" =>
+   resultado <= a or b; -- or
+   --
+	WHEN "100" => -- verificação de beq (branch equal)
+   IF (a<b xor a>b) THEN 
+   resultado <= x"0001"; -- flag para a < b ou a > b
+   ELSE
+   resultado <= x"0000"; -- flag para a = b
+   END IF;
+	--
+	WHEN OTHERS =>
+	resultado <= a + b; -- add
+	--
+	END CASE;
+END PROCESS;
+	zero <= '1' when resultado = x"0000" else '0';
+	ula_resultado <= resultado;
+END Comportamento;
